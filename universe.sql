b425ba087f1324e23013966c43e3281c7bde6459
Gerardo Zaldana <gzaldanaruiz@yahoo.com>
2026-08-15 22:11:27 -0600

    Add sqls files

---
+++ universe.sql
@@ -0,0 +1,3 @@
+pg_dump: error: connection to database "universe" failed: could not connect to server: Connection refused
+	Is the server running locally and accepting
+	connections on Unix domain socket "/var/run/postgresql/.s.PGSQL.5432"?
